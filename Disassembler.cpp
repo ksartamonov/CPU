40ac9c10b3445271d400f5f408e68bd4b7acc565
Kirill Artamonov <artamonov.ks@yandex.ru>
2021-03-11 15:07:35 +0300

    Update Disassembler.cpp

--- Disassembler.cpp
+++ Disassembler.cpp
@@ -19,7 +19,6 @@ int  DisAssemble(int* Commands, FILE* disassembled_cmds, int NumberOfcommands)
 
   assert(Labels != NULL);
   assert(disassembled_cmds != NULL);
-  //printf("Commands amount = %d\n", NumberOfcommands);
 
   for (int com_num = 0; com_num < NumberOfcommands; com_num++)
   {
@@ -30,36 +29,25 @@ int  DisAssemble(int* Commands, FILE* disassembled_cmds, int NumberOfcommands)
     }
   }
 
-  for (int i = 0 ; i < NumberOfcommands; i ++)
-  {
-    printf("Labels[%d] = %d\n", i , Labels[i]);
-  }
-
-  //return 0;
-  // int Commands_Counter = 0; // TODO: счетчик идет и считает команды, так удобно вставить ссылку
-  int LabelsNum = label_idx; //  Присваиваем последнему значению счетчика
+  int LabelsNum = label_idx; //  Присваиваем кличество меток  последнему значению счетчика
 
   int com_num = 0;
   while (PC < NumberOfcommands)
   {
-    // printf("PC = %d\n", PC);
-
     int new_PC = Command_Decoder(Commands[PC], Commands[PC+1], disassembled_cmds, PC, LabelsNum, Labels, com_num);
 
-    //printf("NEW PC = %d\n", new_PC);
-    com_num++;
+    com_num++; // переход к следующей команде
 
     if (new_PC == WRONG_COMMAND)
           {
           printf("Wrong command on the %d position: %d\n", PC, Commands[PC]);
           PC++;
-          // printf("PC = %d\n", PC);
         }
+
     else
-    {
-      PC = new_PC;
-    }
+          PC = new_PC;
   }
+
   return DISASSEMBLED_SUCCESFULLY;
 }
 
@@ -78,8 +66,6 @@ int main(int argc, char* argv [])
 
   int Lines_Amount = CounterOfLines(buf, SizeOfFile);
 
-  //printf("[debug]LINES AMOUNT: %d\n", Lines_Amount);
-
   char** P_Lines = (char**) calloc (Lines_Amount, sizeof (char*));
 
   assert(P_Lines != NULL);
@@ -93,15 +79,10 @@ int main(int argc, char* argv [])
     cmds[idx] = atoi(P_Lines[idx]);
   }
 
-  // for (int i = 0 ; i < Lines_Amount; i ++)
-  // {
-  //   printf("cmds[%d] = %d\n", i , cmds[i]);
-  // }
-
   FILE* DISASSEMBLED_CMDS = fopen("disassembled_cmds.txt", "w");
 
   if ( DisAssemble(cmds, DISASSEMBLED_CMDS, Lines_Amount) == DISASSEMBLED_SUCCESFULLY)
-        printf("Disassembled well!\n");
+        printf("Disassembled successfully!\n FROM: %s \n INTO: disassembled_cmds.txt\n", argv[1]);
 
   return 0;
 }
@@ -111,22 +92,11 @@ int main(int argc, char* argv [])
 int Command_Decoder(int cmd1, int cmd2, FILE* disassembled_cmds, int PC, int LabelsAmount, int* Labels, int cmd_nmbr)
 {
 int cur_PC = PC;
-// printf("PC: %d\n", PC);
+
 int lab_num = IsLabel(PC, disassembled_cmds, LabelsAmount, Labels, cmd_nmbr);
-printf("LABNUM: %d\n", lab_num);
-// if (lab_num >= 0) { fprintf(disassembled_cmds, ":%d\n", Labels[lab_num]); PC ++; return PC; }
-// if (lab_num >= 0)
-// {
-// PC++;
-// }
-
-//printf("AFTER IsLabel PC: %d\n", PC);
-
 
   if ( cmd1 == CMD_PUSH )
   {
-    // printf("DOING PUSH!\n");
-    // printf("PC: %d\n", PC+1);
     fprintf(disassembled_cmds, "PUSH %d\n", cmd2); PC += 2; return PC;
   }
 
@@ -262,14 +232,10 @@ printf("LABNUM: %d\n", lab_num);
 
 int IsLabel (int PC, FILE* disassembled_cmds, int LabelsAmount, int* Labels, int cmd_nmbr)
 {
-  // if ( cmd == CMD_PUSH || cmd == CMD_JMP || cmd == CMD_JB || cmd == CMD_JBE || cmd == CMD_JA || cmd == CMD_JAE || cmd == CMD_JE || cmd == CMD_JNE )
-  // PC++;
-  printf("НА ВХОД ПОЛУЧИЛ PC = %d\n", PC);
   for (int i = 0; i < LabelsAmount; i ++)
   {
     if (Labels[i] == cmd_nmbr)
     {
-      printf("МЕТКА НАШЛАСЬ\n");
       fprintf(disassembled_cmds, ":%d\n", Labels[i]);
       return i;
     }
